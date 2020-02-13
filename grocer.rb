21096a2397fa9a51828732a016f0699f65f4252c
Joseph Chavez <rommelchavez99@gmail.com>
2020-02-13 18:16:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,36 +1,74 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  counter=0
+  while counter < collection.length
+    if collection[counter][:item]== name
+      return collection[counter]
+    end
+    counter += 1
+  end
 end
-
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  new_cart=[]
+  counter=0
+  while counter < cart.length
+    new_cart_item = find_item_by_name_in_collection(cart[counter][:item],new_cart)
+    if new_cart_item != nil
+      new_cart_item[:count] += 1
+    else
+      new_cart_item = {
+        :item => cart[counter][:item],
+        
+      }
 end
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+items_in_the_cart = {
+    "AVOCADO" => {:price => 3.0, :clearance => true, :count => 3},
+    "KALE"    => {:price => 3.0, :clearance => false, :count => 1}
+  }
+  item_in_the_coupon = [{:item => "AVOCADO", :num => 2, :cost => 5.0}]
+  the_expected_outcome = {
+    "AVOCADO" => {:price => 3.0, :clearance => true, :count => 1},
+    "KALE"    => {:price => 3.0, :clearance => false, :count => 1},
+    "AVOCADO W/COUPON" => {:price => 5.0, :clearance => true, :count => 1},
+  }
+  coupons.each do |coupon|
+    name = coupon[:item]
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] += 1
+      else
+        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+      end
+      cart[name][:count] -= coupon[:num]
+    end
+  end
+  return cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+    cart.collect do |item, item_description|
+    if item_description[:clearance] == true
+      # 20% of something, multiply by 20%, get a number, then subtract it from original
+      twenty_percent = item_description[:price] * 0.2
+      item_description[:price] -= twenty_percent
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  cart_1 = consolidate_cart(cart: cart)
+  # cart_1["BEETS"][:price]
+  cart_2 = apply_coupons(cart: cart_1, coupons: coupons)
+  cart_3 = apply_clearance(cart: cart_2)
+  # coupon_1 = coupons
+  total = 0
+  cart_3.each do |name, properties|
+     total += properties[:price] * properties[:count]
+   end
+   total = total * 0.9 if total > 100
+   total
+end
 end
